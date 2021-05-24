7c6adf6837c2b4f52ff34c04f5e45200233a482b
wedersonf <wedymf@hotmail.com>
2021-05-23 23:57:09 -0300

    nextauth

---
+++ src/utils/withSSRAuth.ts
@@ -0,0 +1,63 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetStaticPropsResult } from "next";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+import decode from 'jwt-decode';
+import { validateUserPermissions } from "./validateUserPermissions";
+
+type WithSSRAuthOptions = {
+  permissions?: string[];
+  roles?: string[];
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: WithSSRAuthOptions) {
+  return async (ctx: GetServerSidePropsContext): Promise<GetStaticPropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    const token = cookies['nextauth.token'];
+
+    if (options) {
+      const user = decode<{ permissions: string[], roles: string[]}>(token);
+      const { permissions, roles } = options
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles,
+      })
+
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false,
+          }
+        }
+      }
+    }
+
+
+    if (!cookies['nextauth.token']) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        }
+      }
+    }
+
+    try {
+      return await fn(ctx);
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token');
+        destroyCookie(ctx, 'nextauth.refreshToken')
+    
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          }
+        }
+      }
+    }
+  }
+}
